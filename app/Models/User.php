04c868a7b61c380026e3fea35cefe66b758166f7
Lorenzo <lorenzocardoso@hotmail.com>
2021-09-30 09:58:25 -0300

    Adicionando relacionamentos, e fazendo migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone'
     ];
+
+    public function Schedules(){
+        return $this->hasMany('App\Models\Schedule');
+    }
 
     /**
      * The attributes that should be hidden for serialization.
---
+++ database/migrations/2021_09_30_124240_create_schedules_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->dateTime('day');
+            $table->float('hour');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules', function (Blueprint $table) {
+            $table->dropForeign ('users_user_id_foreign');
+        });
+        Schema::dropIfExists('schedules');
+    }
+}

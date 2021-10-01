697e786f4f6e4fbd8da771fbb57e53b590a9ced3
Lorenzo <lorenzocardoso@hotmail.com>
2021-10-01 08:31:00 -0300

    Adicionando botões de agendamento

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Bem-vindo {{auth()->user()->name}}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,16 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <div class="container">
+                        <div class="row p-4">
+                            <div class=" col-12 d-flex justify-content-center mb-2">
+                                <button class="btn btn-success btn-lg">Agendar Horário</button>
+                            </div>
+                            <div class=" col-12 d-flex justify-content-center">
+                                <button class="btn btn-primary btn-lg">Meus Agendamentos</button>
+                            </div>
+                        </div> 
+                    </div>
                 </div>
             </div>
         </div>
